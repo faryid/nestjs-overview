b9f67d41dd486aac69c597d278ebf80673318838
faryid <m.akbar.alfaryid@gmail.com>
2025-09-28 17:06:09 +0700

    Implement sign in services

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService, private jwtService: JwtService) {}
+
+  // Simple sign in service
+  async signIn(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const { password, ...result } = user;
+    // TODO: Generate a JWT and return it here
+    // instead of the user object
+    return result;
+  }
+
+  // JWT sign in service
+  async signInJwt(username: string, pass: string): Promise<{ acces_token: string }> {
+    const user = await this.usersService.findOne(username);
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const payload = { sub: user.userId , username: user.username };
+    return { acces_token: await this.jwtService.signAsync(payload) };
+  }
+
+  // Simple way to validate a user
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
