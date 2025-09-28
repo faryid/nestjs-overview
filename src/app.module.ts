a6f6674256f79320b14b7d6f5d3c8ac7b51ed058
faryid <m.akbar.alfaryid@gmail.com>
2025-09-28 16:48:31 +0700

    Create simple logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HorsesModule } from './horses/horses.module';
 import { DogsController } from './dogs/dogs.controller';
 import { DogsService } from './dogs/dogs.service';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [HorsesModule],
   controllers: [AppController, DogsController],
   providers: [AppService, DogsService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes({ 
+      path: 'dogs', 
+      method: RequestMethod.POST
+    });
+  }
+}
