39ac14687496fea091160c93ad56ff6b700de8dc
faryid <m.akbar.alfaryid@gmail.com>
2025-09-28 17:35:55 +0700

    Implement some controllers

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Post, HttpCode, HttpStatus, UseGuards, Get, Request } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './guards/auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login/v1')
+  signIn(@Body() signInDto: Record<string, any>) {
+    return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login/jwt/v1')
+  signInJwt(@Body() signInDto: Record<string, any>) {
+    return this.authService.signInJwt(signInDto.username, signInDto.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
