aaeb6610d8aacfe96d0a89f87fb2c385c4f3b492
faryid <m.akbar.alfaryid@gmail.com>
2025-10-01 10:24:03 +0700

    Create LocalStrategy and JwtStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,17 +3,21 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
-    UsersModule, 
+    UsersModule,
+    PassportModule,
     JwtModule.register({
       global: true,
       secret: 'secret',
       signOptions: { expiresIn: '60s' }
     })
   ],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
   exports: [AuthService]
 })
