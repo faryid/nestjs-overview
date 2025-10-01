e40e3ae8ac1c934cbb445893277eda3e4024ccb5
faryid <m.akbar.alfaryid@gmail.com>
2025-10-01 10:49:29 +0700

    Implement Passport strategy authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Post, HttpCode, HttpStatus, UseGuards, Get, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './guards/auth.guard';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -19,8 +21,26 @@ export class AuthController {
   }
 
   @UseGuards(AuthGuard)
-  @Get('profile')
+  @Get('jwt/profile')
   getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login/passport/v1')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  // @UseGuards(AuthGuardPassport('local'))
+  // @Post('logout/passport/v1')
+  // async logout(@Request() req) {
+  //   return req.logout();
+  // }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('passport/profile')
+  async getPassportProfile(@Request() req) {
     return req.user;
   }
 }
